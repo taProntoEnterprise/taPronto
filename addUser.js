b6cec37b444cdbe7cd93fe57bfa4aa91af41679a
tiaraju <tiarajums@gmail.com>
2016-02-13 17:11:51 -0300

    adding user model and adduser route

---
+++ addUser.js
@@ -0,0 +1,29 @@
+var http = require('http');
+
+var config = {
+    hostname: 'localhost',
+    port: 3000,
+    path: '/users/adduser',
+    method: 'POST',
+    headers: {"Content-Type": "application/json"}
+};
+
+
+var req = http.request(config, function(res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        console.log('BODY: ' + chunk);
+    });
+});
+
+req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+});
+
+user= {}
+user.username="tiaraju";
+user.password="tiaraju";
+req.write(JSON.stringify(user));
+req.end();
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,44 @@
 var express = require('express');
+var User = require('../models/user.js');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/',function(req,res){
+	var error = {};
+	var result = {};
+
+	User.find(function(err,doc){
+		if(err){
+                res.contentType('application/json');
+                res.status(500);
+                error.code = err.code;
+                error.message = err.message;
+
+            }else{
+                result = doc;
+                res.contentType('application/json');
+                res.send(JSON.stringify({"result":result, "error":error}));
+            }
+	});
+
+
 });
+router.post('/adduser', function(req, res) {
+	var user = new User(req.body);
+	var error= {};
+	var result = {};
+	user.save(function(err) {
+		if (err) {
+			error.code = err.code;
+			error.message = err.message;
+      		//11000: duplicated key
+      		error.code == 11000 ? res.status(409) : res.status(500);
+      	}else{
+		      res.status(201);
+		      //result.uri = "/users/user/" + user.username;
+		}
+  		res.send(JSON.stringify({"result": result, "error": error}));
+	});
+});
+
 
 module.exports = router;
