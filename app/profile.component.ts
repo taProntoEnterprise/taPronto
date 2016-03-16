d351cab2718fccd5a16a0e6c3a560d148102e4d3
Lurian <luclurian@gmail.com>
2016-03-15 23:03:27 -0300

    Provider service

--- app/profile.component.ts
+++ app/profile.component.ts
@@ -1,5 +1,7 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {LOGGED_USER} from './user.service';
+import {ProviderService} from './provider.service';
+import {AlertService} from './alert.service';
 
 
 // Funciona como declaracao do controller/diretiva, tudo é diretiva no angular 2.0,
@@ -7,12 +9,14 @@ import {LOGGED_USER} from './user.service';
 // e acessarem 'certas' coisas deles. Bem vago.
 @Component({
     selector: 'profile', // nome utilizado no HTML para importar a diretiva
-    templateUrl: 'views/profile.html'
+    templateUrl: 'views/profile.html',
+    providers: [ProviderService]
 })
 
 // Aqui eh a função em si do controller/diretiva
-export class ProfileComponent {
-
+export class ProfileComponent implements OnInit {
+	constructor(private _providerService: ProviderService,
+		private _alertService: AlertService) { }
 	public user = LOGGED_USER;
 
 
@@ -32,5 +36,24 @@ export class ProfileComponent {
 
 	addNewEmail() {
 		this.provider.emails.push("");
+	}
+
+	registerProvider(){
+		this._providerService.registerProvider(this.provider).subscribe(
+			provider => this.provider = provider,
+			error => this.alertaErro(error))
+	}
+
+	getProvider(){
+		this._providerService.getServices().subscribe(
+			provider => this.provider = provider,
+			error => this.alertaErro(error))
+	}
+		alertaErro(error){
+		this._alertService.addErrorAlert(error.message);
+	}
+
+	ngOnInit() {
+		this.getProvider();
 	}
 }
---
+++ app/provider.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from 'angular2/core';
+import {Provider} from './provider';
+import {Http, Response, RequestOptions, Headers} from 'angular2/http';
+import {Observable} from 'rxjs/Observable';
+import { LOGGED_USER } from './user.service';
+
+
+// Velho e bom service do Angular
+@Injectable() // <-- Fucking necessarios esses parenteses, foi dito umas 3 vzs para sempre colocar eles, entao deve ser importante
+export class ProviderService {
+	constructor(private http: Http) { }
+
+	private _providersUrl = 'http://localhost:3000/providers/';
+	
+	getServices() {
+		var uri = this._providersUrl + LOGGED_USER.id 
+		return this.http.get(uri)
+                  .map(res => res.json().result)
+				  .catch(this.handleError);
+	}
+
+	registerProvider(newProvider){
+		var uri = this._providersUrl + LOGGED_USER.id + '/addprovider';
+		let body = JSON.stringify(newProvider);
+		let headers = new Headers({ 'Content-Type': 'application/json' });
+		let options = new RequestOptions({ headers: headers });
+		return this.http.post(uri, body, options)
+                  .map(res => res.json().result)
+				  .catch(this.handleError);
+	}
+
+ 	private handleError (error: Response) {
+	   console.error(error);
+	   return Observable.throw(error.json().error || 'Server error');
+	}
+}
