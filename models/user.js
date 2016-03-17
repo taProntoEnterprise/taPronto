28813a57d0614e103d43de5b4f675378d9e15465
iagoaraujo <joseiago.p.a@gmail.com>
2016-03-16 22:51:00 -0300

    Fixing view and validations of provider

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-//var scrypt = require('scrypt');
+var scrypt = require('scrypt');
 
 var UserSchema = new mongoose.Schema
 (
@@ -12,20 +12,20 @@ var UserSchema = new mongoose.Schema
 
 );
 
-//UserSchema.pre('save',function (next) {
-//	var user = this;
-//	if(!user.isModified('password')) return next();
-//	scrypt.hash(user.password,{"N":16,"r":1,"p":1},64,"",function (err,hash) {
-//		if(err){return next(err);}
-//		user.password = hash.toString('hex');
-//	next(); 
-//	}); 
-//});
+UserSchema.pre('save',function (next) {
+	var user = this;
+	if(!user.isModified('password')) return next();
+	scrypt.hash(user.password,{"N":16,"r":1,"p":1},64,"",function (err,hash) {
+		if(err){return next(err);}
+		user.password = hash.toString('hex');
+	next(); 
+	}); 
+});
 
 UserSchema.methods.verifyPass = function (candidatePassword) {
-//	var user= this;
-//	var candidate = scrypt.hashSync(candidatePassword,{"N":16,"r":1,"p":1},64,"");
-//	return user.password===candidate.toString("hex");
+	var user= this;
+	var candidate = scrypt.hashSync(candidatePassword,{"N":16,"r":1,"p":1},64,"");
+	return user.password===candidate.toString("hex");
 	return true;
 };
 
