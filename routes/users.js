f9d12838f83d66222978975492fae1a91edb380e
tiaraju <tiarajums@gmail.com>
2016-03-21 17:03:52 -0300

    refactoring user's routes

--- routes/users.js
+++ routes/users.js
@@ -5,26 +5,7 @@ var Service = require('../models/service.js');
 var mongoose = require('mongoose');
 var router = express.Router();
 
-router.get('/',function(req,res){
-	var error = {};
-	var result = {};
-
-	User.find(function(err,doc){
-		if(err){
-                res.contentType('application/json');
-                res.status(500);
-                error.code = err.code;
-                error.message = err.message;
-
-            }else{
-                result = doc;
-                res.contentType('application/json');
-                res.send(JSON.stringify({"result":result, "error":error}));
-            }
-	});
-});
-
-router.post('/adduser', function(req, res) {
+router.post('/', function(req, res) {
 	var user = new User(req.body);
 	var error= {};
 	var result = {};
@@ -36,7 +17,9 @@ router.post('/adduser', function(req, res) {
       		error.code == 11000 ? res.status(409) : res.status(500);
       	}else{
 		      res.status(201);
-		      //result.uri = "/users/user/" + user.username;
+		      var data = user.toObject();
+		      delete data.password;
+		      result.data = data;
 		}
   		res.send(JSON.stringify({"result": result, "error": error}));
 	});
