a9c3bc485cd5af594ea681993eea481caa4242a1
Lurian <luclurian@gmail.com>
2016-03-26 19:39:06 -0300

    Criando service de orders

--- app/add-order.component.ts
+++ app/add-order.component.ts
@@ -1,46 +1,71 @@
 import { Component, OnInit } from 'angular2/core';
 import { ServiceService } from './service.service';
+import { OrderService } from './order.service';
 import { RequestOptions } from 'angular2/http';
 import { AlertService } from './alert.service';
+import { LOGGED_USER } from './user.service';
 import { ServiceListComponent } from './service-list.component';
 
 @Component({
 	selector: 'addOrder',
 	templateUrl: 'views/addOrder.html',
-	providers: [ServiceService],
+	providers: [ServiceService, OrderService],
 	directives: [ServiceListComponent]
 })
 
 export class AddOrderComponent implements OnInit {
 	constructor(private _alertService: AlertService,
-		private _serviceService: ServiceService) { }
+		private _serviceService: ServiceService,
+		private _orderService: OrderService) { }
 
+	private dataAtual = new Date();
 	public selectedService;
 	public serviceList;
 	public newOrder = {
 		description: "",
-		price: 0
+		price: 0,
+		code: this.dataAtual.getMilliseconds(),
+		service: undefined,
+		client: undefined,
+		status: "Em andamento"
 	};
+
+	registerOrder(){
+		this.newOrder.service = this.selectedService._id;
+		this.newOrder.client = LOGGED_USER.id;
+		this._orderService.registerOrder(this.newOrder).subscribe(
+			order => this.afterSuccessfulRegister(),
+			error => this.alertaErro(error));
+	}
+
+	afterSuccessfulRegister() {
+		this._alertService.addSuccessAlert("Pedido cadastrado com sucesso");
+		this.back();
+	}
 
 	selectService(service){
 		this.selectedService = service;
-	}
+	};
 
 	deselectService() {
 		this.selectedService = undefined;
-	}
+	};
 
 	back() {
 		window.history.back();
-	}
+	};
 
 	hasSelectedService(){
 		return this.selectedService !== undefined;
+	};
+
+	alertaErro(error){
+		this._alertService.addErrorAlert(error.message);
 	}
 
 	ngOnInit(){
 		this._serviceService.getServices().subscribe(
 			services => this.serviceList = services,
 			error => console.log(error));
-	}
+	};
 }
--- app/order.component.ts
+++ app/order.component.ts
@@ -2,19 +2,20 @@ import { Component, OnInit, Input } from 'angular2/core';
 import { Router } from 'angular2/router';
 import { AlertService } from './alert.service';
 import { Order } from './models/order';
-// import { ServiceService } from './service.service';
+import { OrderService } from './order.service';
 import { OrderListComponent } from './order-list.component';
 
 @Component({
     selector: 'order',
     templateUrl: 'views/order.html',
-    // providers: [OrderService],
+    providers: [OrderService],
 	directives: [OrderListComponent]
 })
 
 export class OrderComponent implements OnInit {
 	constructor(private _router: Router,
-		private _alertService: AlertService) { }
+		private _alertService: AlertService,
+		private _orderService: OrderService) { }
 
 	public orderList;
 
@@ -23,11 +24,8 @@ export class OrderComponent implements OnInit {
 	}
 	
 	ngOnInit() {
-	//	this._serviceService.getServices().subscribe(
-	//		services => this.serviceList = services,
-	//		error => console.log(error));
-		this.orderList = [{ code: "1", description: "Pedido 1" }, 
-		{code: "2", description: "Pedido 2"},
-		{code: "3", description: "Pedido 3"}  ]
+		this._orderService.getOrders().subscribe(
+			orders => this.orderList = orders,
+			error => console.log(error));
 	}
 }
